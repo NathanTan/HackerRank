ee861772721100b106d57b6ec6638dd15bb9b2cc
NathanTan <nathan1011@protonmail.com>
2022-02-20 20:00:17 -0800

    Another one

--- code/meta-prep/Character Frequency.ts
+++ code/meta-prep/Character Frequency.ts
@@ -27,3 +27,62 @@ word: "hello", c: ""
 output: 0
 Tips: Think about how to solve this without using an imported library, like Counter in collections if using python.
 */
+const test1 = {
+    word: "Mississippi", c: "s",
+    output: 4
+}
+const test2 = {
+    word: "Rainbow", c: "j",
+    output: 0
+}
+const test3 = {
+    word: "Mirror", c: "m",
+    output: 0
+}
+const test4 = {
+    word: "", c: "c",
+    output: 0
+}
+
+const test5 = {
+    word: "hello", c: "",
+    output: 0
+}
+
+const returnCharNum = (word: String, ch: String): Number => {
+    let count = 0
+    word.split("").forEach(c => {
+        if (c === ch) {
+            count++
+        }
+    })
+
+    return count
+}
+
+
+{
+    let result = returnCharNum(test1.word, test1.c)
+    console.log(result)
+    console.log(test1.output)
+}
+{
+    let result = returnCharNum(test2.word, test2.c)
+    console.log(result)
+    console.log(test2.output)
+}
+{
+    let result = returnCharNum(test3.word, test3.c)
+    console.log(result)
+    console.log(test3.output)
+}
+{
+    let result = returnCharNum(test4.word, test4.c)
+    console.log(result)
+    console.log(test4.output)
+}
+{
+    let result = returnCharNum(test5.word, test5.c)
+    console.log(result)
+    console.log(test5.output)
+}
